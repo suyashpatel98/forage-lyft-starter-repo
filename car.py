5c5ca8c7e76899138f5fae84ad9a0b9559a496cb
Suyash Patel <suyash@Suyashs-MacBook-Air.local>
2022-05-25 15:55:36 -0700

    Adding the tires functionality; Some unit tests fail but the behaviour is correct

--- car.py
+++ car.py
@@ -2,10 +2,10 @@ from abc import ABC
 
 
 class Car(ABC):
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery, tires):
         self.engine = engine
         self.battery = battery
+        self.tires = tires
 
     def needs_service(self):
-        print(str(self.engine.needs_service()) + " " + str(self.battery.needs_service()))
-        return self.engine.needs_service() or self.battery.needs_service()
+        return self.engine.needs_service() or self.battery.needs_service() or self.tires.needs_service()
